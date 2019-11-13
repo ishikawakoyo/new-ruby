fc87f74336a55d3cf8c2825adc29054638523803
ishikawakoyo <57532348+ishikawakoyo@users.noreply.github.com>
2019-11-13 16:19:22 +0900

    Update array_prac.rb

--- array_prac.rb
+++ array_prac.rb
@@ -2,14 +2,13 @@ array  = [1,2,3,4]
 array[4] = "e"
 
 p array
-#["a", "b", "c", "d", "e"]
 
 array.push("f")
 p array
-#["a", "b", "c", "d", "e", "f"]
+
 
 array << 'hoge'
 p array
-#["a", "b", "c", "d", "e", "f", "hoge"]
+
 
 p array[0]
